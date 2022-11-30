74036d9cf335d75a2072521d05ee1d25011cb6c8
swj9707 <swj1265@naver.com>
2022-11-30 20:52:51 +0900

    Initial commit

---
+++ src/main/java/com/swj9707/twittercloneadmin/TwitterCloneAdminApplication.java
@@ -0,0 +1,13 @@
+package com.swj9707.twittercloneadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swj9707/twittercloneadmin/TwitterCloneAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swj9707.twittercloneadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
